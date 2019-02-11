07860225004b98ebdc34cca26e14972cc1bef502
Alper Silistre <alpersilistre@gmail.com>
2019-02-12 01:22:29 +0300

    Forum controller and view added to the project.

---
+++ LambdaForums/Controllers/ForumController.cs
@@ -0,0 +1,34 @@
+﻿using LambdaForums.Data;
+using LambdaForums.Models.ForumViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace LambdaForums.Controllers
+{
+    public class ForumController : Controller
+    {
+        private readonly IForum _forumService;
+
+        public ForumController(IForum forumService)
+        {
+            _forumService = forumService;
+        }
+
+        public IActionResult Index()
+        {
+            var forums = _forumService.GetAll().Select(x => new ForumListingModel
+            {
+                Id = x.Id,
+                Title = x.Title,
+                Description = x.Description
+            });
+
+            var indexModel = new ForumIndexModel
+            {
+                ForumList = forums
+            };
+
+            return View(indexModel);
+        }
+    }
+}
